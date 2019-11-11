fce0dfad7d3d5d4c5c2df01de055be0685fa4f1c
italolsimas <italosimas@outlook.com>
2019-11-11 10:54:10 -0300

    Sext commit: Replace Temp with Query

--- Costumer.java
+++ Costumer.java
@@ -20,10 +20,7 @@ public String statement() {
    Enumeration rentals = _rentals.elements();
    String result = "Rental Record for " + getName() + "\n";
    while (rentals.hasMoreElements()) {
-      double thisAmount = 0;
       Rental each = (Rental) rentals.nextElement();
-
-      thisAmount = each.getCharge();
 
       // add frequent renter points
       frequentRenterPoints ++;
@@ -33,8 +30,8 @@ public String statement() {
 
       //show figures for this rental
       result += "\t" + each.getMovie().getTitle()+ "\t" +
-          String.valueOf(thisAmount) + "\n";
-      totalAmount += thisAmount;
+          String.valueOf(each.getCharge()) + "\n";
+      totalAmount += each.getCharge();
    } 
    //add footer lines
    result +=  "Amount owed is " + String.valueOf(totalAmount) + "\n";
